d6d95a1c22d7bad37b4db7ee4d65246a42aea1c2
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-07-09 09:49:10 -0700

    lb module

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  default = "production"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "alb_name" {}
+
+variable "vpc_id" {}
+
+variable "app" {}
+
+variable "organization" {}
+
+variable "public_subnet_ids" {}
+
+
